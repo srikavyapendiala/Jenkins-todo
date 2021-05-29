9d63d7a4180c4d207cf67110dffafb1e6b96a396
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-29 14:26:01 +0530

    #commit

--- seed.groovy
+++ seed.groovy
@@ -5,8 +5,8 @@ folder('CI-Pipelines') {
 
 pipelineJob('CI-Pipelines/login') {
     configure { flowdefinition ->
-        flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
+        flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+            'scm'(class: 'hudson.plugin.git.GitSCM', plugin: 'git') {
                 'userRemoteConfigs' {
                     'hudson.plugin.git.UserRemoteConfig' {
                         'url'('https://github.com/srikavyapendiala/jenkins-todo.git')
