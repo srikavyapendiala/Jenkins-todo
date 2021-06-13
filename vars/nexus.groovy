6f36a4f17f8df648c62d27e8e6038d00fdc8cbed
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-06-13 16:25:01 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -20,11 +20,11 @@ def make_artifacts(APP_TYPE, COMPONENT) {
         def execute_com=sh(returnStdout: true, script: command )
         print execute_com
     }   else if(APP_TYPE == "GO") {
-        command = " zip -r ${FILENAME} login-ci "
+        command = " zip -r ${FILENAME} * "
         def execute_com=sh(returnStdout: true, script: command )
         print execute_com
     }   else if(APP_TYPE == "NODEJS") {
-        command = " zip -r ${FILENAME} node_modules server.js "
+        command = " zip -r ${FILENAME} * "
         def execute_com=sh(returnStdout: true, script: command )
         print execute_com
     }
