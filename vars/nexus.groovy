b7085bebc4e180538ca86cc39e19e283e3b761e2
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-06-13 16:01:08 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,44 +1,49 @@
-def nexus(COMPONENT) {
-  get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-  def get_branch_exec=sh(returnStdout: true, script: get_branch)
-  def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+def nexus (COMPONENT) {
+    get_branch ="env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
-  command = "curl -f -v -u admin:kavya --upload-file ${FILENAME} http://172.31.7.43:8081/repository/${COMPONENT}/${FILENAME}"
-  def execute_state=sh(returnStdout: true, script: command)
+    command = "curl -v -u admin:admin --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
+    def execute_state=sh(returnStdout: true, script: command)
 }
 
 def make_artifacts(APP_TYPE, COMPONENT) {
-  get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-  def get_branch_exec=sh(returnStdout: true, script: get_branch)
-  println("abc${get_branch_exec}abc")
-  def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
-  if(APP_TYPE == "NGINX") {
-    command = "cd static && zip -r ../${FILENAME} *"
-    def execute_com=sh(returnStdout: true, script: command)
-    print execute_com
-  } else if(APP_TYPE == "NODEJS") {
-    command = "zip -r ${FILENAME} node_modules server.js"
-    def execute_com=sh(returnStdout: true, script: command)
-    print execute_com
-  } else if(APP_TYPE == "JAVA") {
-    command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
-    def execute_com=sh(returnStdout: true, script: command)
-    print execute_com
-  } else if(APP_TYPE == "PYTHON") {
-    command = "zip -r ${FILENAME} payment.ini payment.py rabbitmq.py requirements.txt"
-    def execute_com=sh(returnStdout: true, script: command)
-    print execute_com
-  }
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+    if(APP_TYPE == "NGINX" ) {
+        command = " zip -r ${FILENAME} * "
+        def execute_com=sh(returnStdout: true, script: command )
+        print execute_com
+    }   else if(APP_TYPE == "MAVEN"){
+        command = " cp target/*.jar user.jar && zip -r ${FILENAME} user.jar "
+        def execute_com=sh(returnStdout: true, script: command )
+        print execute_com
+    }   else if(APP_TYPE == "GO") {
+        command = " zip -r ${FILENAME} login-ci "
+        def execute_com=sh(returnStdout: true, script: command )
+        print execute_com
+    }   else if(APP_TYPE == "NODEJS") {
+        command = " zip -r ${FILENAME} node_modules server.js "
+        def execute_com=sh(returnStdout: true, script: command )
+        print execute_com
+    }
+
 }
 
 def code_build(APP_TYPE, COMPONENT) {
-  if(APP_TYPE == "NODEJS") {
-    command = "npm install"
-    def execute_com=sh(returnStdout: true, script: command)
-    print execute_com
-  } else if(APP_TYPE == "JAVA") {
-    command = "mvn clean package"
-    def execute_com=sh(returnStdout: true, script: command)
-    print execute_com
-  }
+    if(APP_TYPE == "MAVEN"){
+        command = " mvn clean package "
+        def execute_com=sh(returnStdout: true, script: command )
+        print execute_com
+    }   else if(APP_TYPE == "GO") {
+        command = " go get github.com/dgrijalva/jwt-go && go get github.com/labstack/echo && go get github.com/labstack/echo/middleware && go get github.com/labstack/gommon/log && go get github.com/openzipkin/zipkin-go && go get github.com/openzipkin/zipkin-go/middleware/http && go get github.com/openzipkin/zipkin-go/reporter/http &&  go build "
+        def execute_com=sh(returnStdout: true, script: command )
+        print execute_com
+    }   else if(APP_TYPE == "NODEJS") {
+        command = " npm install "
+        def execute_com=sh(returnStdout: true, script: command )
+        print execute_com
+    }
+
 }
