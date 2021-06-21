6c975266305e24e3917b908726f1895d98ef8d38
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-06-21 14:43:41 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -30,3 +30,24 @@ for (i in 0..count) {
     }
   }
 }
+
+pipelineJob("Deployment Pipeline") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/srikavyapendiala/jenkins-todo.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile-Deployment')
+      'lightweight'(true)
+    }
+  }
+}
