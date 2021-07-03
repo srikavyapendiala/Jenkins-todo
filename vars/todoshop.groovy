a32c45b36537a492eac038ad803e62668fecfd24
srikavyapendiala <srikavya@zelarsoft.com>
2021-07-03 17:27:11 +0530

    #commit

--- vars/todoshop.groovy
+++ vars/todoshop.groovy
@@ -1,15 +1,21 @@
+
 def call(Map params = [:]) {
+  // Start Default Arguments
   def args = [
           NEXUS_IP               : '172.31.12.183',
   ]
   args << params
+
+  // End Default + Required Arguments
   pipeline {
     agent {
       label "${args.SLAVE_LABEL}"
     }
+
     triggers {
       pollSCM('* * * * 1-5')
     }
+
     environment {
       COMPONENT     = "${args.COMPONENT}"
       NEXUS_IP      = "${args.NEXUS_IP}"
@@ -17,15 +23,20 @@ def call(Map params = [:]) {
       SLAVE_LABEL   = "${args.SLAVE_LABEL}"
       APP_TYPE      = "${args.APP_TYPE}"
     }
+
     stages {
+
       stage('Build Code & Install Dependencies') {
         steps {
+          sh 'env'
           script {
             build = new nexus()
             build.code_build("${APP_TYPE}", "${COMPONENT}")
           }
         }
       }
+
+
       stage('Prepare Artifacts') {
         steps {
           script {
@@ -34,6 +45,7 @@ def call(Map params = [:]) {
           }
         }
       }
+
       stage('Upload Artifacts') {
         steps {
           script {
@@ -42,6 +54,18 @@ def call(Map params = [:]) {
           }
         }
       }
+
+      stage('Deploy to Dev Env') {
+        steps {
+          script {
+            get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+            env.get_branch_exec=sh(returnStdout: true, script: get_branch)
+          }
+          build job: 'Deployment Pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: "${get_branch_exec}")]
+        }
+      }
+
     }
+
   }
 }
