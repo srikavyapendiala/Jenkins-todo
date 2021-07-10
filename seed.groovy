3b82bb1a701fd2ea50bf1136445a4c70eea08eaa
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-10 13:41:20 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -3,34 +3,41 @@ folder('CI-Pipelines') {
   description('CI Pipelines')
 }
 
-def component = ["frontend","users","login","todo","redis"];
+def component = ["frontend","todo","login","users"];
 
 def count=(component.size()-1)
 for (i in 0..count) {
   def j=component[i]
   pipelineJob("CI-Pipelines/${j}-ci") {
     configure { flowdefinition ->
+      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+        'triggers' {
+          'hudson.triggers.SCMTrigger' {
+            'spec'('*/2 * * * 1-5')
+            'ignorePostCommitHooks'(false)
+          }
+        }
+      }
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/srikavyapendiala/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')  
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-               'name'('*/tags/*')
+              'name'('*/tags/*')
             }
           }
         }
-        'scriptPath'('Jenkinsfile')
+        'scriptPath'('Jenkinsfile-Docker')
         'lightweight'(true)
       }
     }
   }
 }
-
 
 pipelineJob("Deployment Pipeline") {
   configure { flowdefinition ->
@@ -38,7 +45,7 @@ pipelineJob("Deployment Pipeline") {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/srikavyapendiala/jenkins-todo.git')
+            'url'('https://github.com/srikavyapendiala/jenkins.git')
           }
         }
         'branches' {
@@ -52,6 +59,4 @@ pipelineJob("Deployment Pipeline") {
     }
   }
 }
-
-
 
