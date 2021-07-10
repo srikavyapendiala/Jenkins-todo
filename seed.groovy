20b5fc051635f8a8126dbc1362a827e600a214fc
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-10 14:03:05 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -59,44 +59,4 @@ pipelineJob("Deployment Pipeline") {
     }
   }
 }
-
-folder('Docker-Pipelines') {
-  displayName('Docker Pipelines')
-  description('Docker Pipelines')
-}
-
-def component = ["frontend","todo","login","users"];
-
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("Docker-Pipelines/${j}-Docker") {
-    configure { flowdefinition ->
-      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-          'hudson.triggers.SCMTrigger' {
-            'spec'('*/2 * * * 1-5')
-            'ignorePostCommitHooks'(false)
-          }
-        }
-      }
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/srikavyapendiala/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile-Docker')
-        'lightweight'(true)
-      }
-    }
-  }
 }
