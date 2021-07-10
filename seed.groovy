f1ed9952e1636f5eb06911b5b3a3cb3ffb89480e
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-07-10 13:36:43 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -55,38 +55,3 @@ pipelineJob("Deployment Pipeline") {
 
 
 
-folder('CI-Pipelines') {
-  displayName('CI Pipelines')
-  description('CI Pipelines')
-}
-
-def component = ["frontend","users","login","todo","redis"];
-
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}-ci") {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/srikavyapendiala/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')  
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-               'name'('*/tags/*')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile-Docker')
-        'lightweight'(true)
-      }
-    }
-  }
-}
-
-
-
