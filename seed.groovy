27287ac944e2d9029b9f2d64921c07f6220e6438
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-08-09 11:31:48 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,62 +1,102 @@
 folder('CI-Pipelines') {
-  displayName('CI Pipelines')
-  description('CI Pipelines')
+    displayName('CI Pipelines')
+    description('CI Pipelines')
 }
 
-def component = ["frontend","todo","login","users"];
+def component = ["frontend","users","login","todo"];
 
 def count=(component.size()-1)
 for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}-ci") {
-    configure { flowdefinition ->
-      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-          'hudson.triggers.SCMTrigger' {
-            'spec'('*/2 * * * 0-6')
-            'ignorePostCommitHooks'(false)
-          }
-        }
-      }
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/srikavyapendiala/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+    def j=component[i]
+    pipelineJob("CI-Pipelines/${j}-ci") {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('*/2 * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
             }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/srikavyapendiala/'+j+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/tags/*')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
             }
-          }
         }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
     }
-  }
 }
-
 pipelineJob("Deployment Pipeline") {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/srikavyapendiala/jenkins-todo.git')
-          }
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/srikavyapendiala/jenkins-todo.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Deployment')
+            'lightweight'(true)
         }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile-Docker')
-      'lightweight'(true)
     }
-  }
 }
 
+
+folder('Pipelines-Docker') {
+    displayName('Pipelines-Docker')
+    description('Pipelines-Docker')
+}
+def Component = ["frontend","users","login","todo"];
+
+def Count=(Component.size()-1)
+for (i in 0..Count) {
+    def j=Component[i]
+
+    pipelineJob("Pipelines-Docker/${j}-ci") {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('*/2 * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+            }
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/srikavyapendiala/'+j+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/tags/*')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile-Docker')
+                'lightweight'(true)
+            }
+        }
+    }
+}
